2cad2a8686eab597c400527b699c26be3960e392
Siva S <sivasbrmni@gmail.com>
2026-06-14 14:25:58 +0530

    chore: trigger frontend deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Deploy trigger: 2026-06-14
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
